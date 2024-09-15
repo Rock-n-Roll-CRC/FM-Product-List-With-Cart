9ace5c1a03ec1fad389f9bf9772d735f76c3a39e
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2024-09-15 20:59:17 +0700

    ♻️ Refactored previous code.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,14 +1,15 @@
 import productsData from "../data/products.json";
 import { v4 as uuidv4 } from "uuid";
 
-const products: Product[] = [];
+// Global Constants
+const ICONS_URL = new URL("../assets/icons.svg", import.meta.url);
 
+// DOM Elements
 const productListElement: HTMLUListElement | null = document.querySelector(
   ".product-list__list",
 );
 
-const iconsURL = new URL("../assets/icons.svg", import.meta.url);
-
+// Product Implementation
 class Product {
   private readonly id = uuidv4();
   private quantity = 0;
@@ -35,81 +36,150 @@ class Product {
       image: this.image,
     };
   }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 0) this.quantity--;
+  }
 }
 
 class ProductUIHandler {
-  constructor(private readonly product: Product) {}
-
-  renderProduct() {
-    const productData = this.product.data;
+  static renderProduct(product: Product) {
     const markup = `
-    <li class="product product-list__product" data-id=${productData.id}>
+    <li class="product product-list__product" data-id=${product.data.id}>
       <div class="product__image-box">
         <img
           srcset="
-            ${productData.image.tablet} 427w,
-            ${productData.image.desktop} 502w,
-            ${productData.image.mobile} 654w
+            ${product.data.image.tablet}  427w,
+            ${product.data.image.desktop} 502w,
+            ${product.data.image.mobile}  654w
           "
           sizes="(max-width: 47.937rem) 654px, (max-width: 63.937rem) 427px, 502px"
-          src="${productData.image.mobile}"
-          alt="${productData.name}"
+          src="${product.data.image.mobile}"
+          alt="${product.data.name}"
           class="product__image"
         />
 
         <button class="button button--type-add-to-cart product__button">
           <svg class="button__icon button__icon--type-add-to-cart">
-            <use href="${iconsURL.toString()}#add-to-cart" />
+            <use href="${ICONS_URL.toString()}#add-to-cart" />
           </svg>
           Add to Cart
         </button>
-        <button
+        <div
           class="button button--type-product-quantity button--hidden product__button"
         >
-          <svg class="button__icon button__icon--type-increment-quantity">
-            <use href="${iconsURL.toString()}#increment-quantity" />
-          </svg>
-          <svg class="button__icon button__icon--type-decrement-quantity">
-            <use href="${iconsURL.toString()}#decrement-quantity" />
-          </svg>
-          ${productData.quantity.toString()}
-        </button>
+          <button
+            class="button button--type-increment-quantity product__button product__button--type-increment-quantity"
+          >
+            <svg
+              class="button__icon button__icon--type-increment-quantity"
+            >
+              <use href="${ICONS_URL.toString()}#increment-quantity" />
+            </svg>
+          </button>
+          <button
+            class="button button--type-decrement-quantity product__button product__button--type-decrement-quantity"
+          >
+            <svg
+              class="button__icon button__icon--type-decrement-quantity"
+            >
+              <use href="${ICONS_URL.toString()}#decrement-quantity" />
+            </svg>
+          </button>
+          ${product.data.quantity.toString()}
+        </div>
       </div>
 
       <div class="product__text-content">
-        <p class="product__category">${productData.category}</p>
-        <p class="product__name">${productData.name}</p>
-        <p class="product__price">$${productData.price.toFixed(2)}</p>
+        <p class="product__category">${product.data.category}</p>
+        <p class="product__name">${product.data.name}</p>
+        <p class="product__price">$${product.data.price.toFixed(2)}</p>
       </div>
     </li>
     `;
 
     productListElement?.insertAdjacentHTML("beforeend", markup);
   }
+
+  static updateQuantity(product: Product) {
+    const productElement: HTMLLIElement | null = document.querySelector(
+      `.product[data-id="${product.data.id}"]`,
+    );
+    const productQuantityNode = productElement?.querySelector(
+      ".button--type-product-quantity",
+    )?.lastChild;
+
+    if (productQuantityNode)
+      productQuantityNode.textContent = product.data.quantity.toString();
+  }
 }
 
-for (const productData of productsData) {
-  const product = new Product(
-    productData.name,
-    productData.category,
-    productData.price,
-    productData.image,
-  );
-  const productUIHandler = new ProductUIHandler(product);
+// Initialising products
+const products: Product[] = productsData.map(
+  (productData) =>
+    new Product(
+      productData.name,
+      productData.category,
+      productData.price,
+      productData.image,
+    ),
+);
+products.forEach((product) => {
+  ProductUIHandler.renderProduct(product);
+});
 
-  products.push(product);
-  // productUIHandler.renderProduct();
-}
+// Utility Functions
+const getProductById = (id: string) => {
+  return products.find((product) => product.data.id === id);
+};
 
+// Listening For Events
 productListElement?.addEventListener("click", (e) => {
-  if ((e.target as Element).matches(".button--type-add-to-cart")) {
-    const productQuantityButton: HTMLButtonElement | null | undefined = (
-      e.target as Element
-    ).parentElement?.querySelector(".button--type-product-quantity");
+  const targetElement = e.target as HTMLElement;
 
-    if (productQuantityButton) {
-      (e.target as Element).classList.add("button--hidden");
-      productQuantityButton.classList.remove("button--hidden");
+  // Hiding/Showing Buttons
+  if (targetElement.closest(".button--type-add-to-cart")) {
+    const productElement: HTMLLIElement | null =
+      targetElement.closest(".product");
+
+    if (productElement) {
+      const addToCartButton: HTMLButtonElement | null =
+        productElement.querySelector(".button--type-add-to-cart");
+      const productQuantityButton: HTMLButtonElement | null =
+        productElement.querySelector(".button--type-product-quantity");
+
+      addToCartButton?.classList.add("button--hidden");
+      productQuantityButton?.classList.remove("button--hidden");
+    }
+  }
+
+  // Handling Product Quantity
+  if (
+    targetElement.closest(".button--type-increment-quantity") ||
+    targetElement.closest(".button--type-decrement-quantity")
+  ) {
+    const productElement: HTMLLIElement | null =
+      targetElement.closest(".product");
+    const productElementId = productElement?.dataset.id;
+
+    if (productElementId) {
+      const product = getProductById(productElementId);
+
+      if (product) {
+        if (targetElement.closest(".button--type-increment-quantity")) {
+          product.incrementQuantity();
+          ProductUIHandler.updateQuantity(product);
+        }
+
+        if (targetElement.closest(".button--type-decrement-quantity")) {
+          product.decrementQuantity();
+          ProductUIHandler.updateQuantity(product);
+        }
+      }
     }
   }
 });
